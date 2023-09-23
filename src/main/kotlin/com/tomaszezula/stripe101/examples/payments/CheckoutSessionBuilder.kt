c6371f8b71a672cccf13caf3ef576d6bfa53b09d
zezutom <zezulatomas@gmail.com>
2023-09-23 09:14:33 +0200

    feat: Examples of checkout session and tiered prices.

--- src/main/kotlin/com/tomaszezula/stripe101/examples/payments/CheckoutSessionBuilder.kt
+++ src/main/kotlin/com/tomaszezula/stripe101/examples/payments/CheckoutSessionBuilder.kt
@@ -1,5 +1,6 @@
 package com.tomaszezula.stripe101.examples.payments
 
+import com.stripe.Stripe
 import com.stripe.model.checkout.Session
 
 typealias LineItem = Map<String, Any>
@@ -21,10 +22,24 @@ class CheckoutSessionBuilder {
             mapOf(
                 "payment_method_types" to listOf("card"),
                 "line_items" to lineItems,
-                "mode" to "payment",
+                "mode" to "subscription",
                 "success_url" to successUrl,
                 "cancel_url" to cancelUrl
             )
         )
     }
+}
+
+fun main() {
+    Stripe.apiKey = "your api key"
+
+    val builder = CheckoutSessionBuilder()
+    val session = builder
+        .addLineItem(priceId = "price_1NtOfQGyWpmEflR8il6mXoFE", quantity = 1)
+        .addLineItem(priceId = "price_1NtOj5GyWpmEflR8UyQMt48n", quantity = 5)
+        .build(
+            successUrl = "https://example.com/success",
+            cancelUrl = "https://example.com/cancel"
+        )
+    println(session.url)
 }
--- src/main/kotlin/com/tomaszezula/stripe101/examples/payments/TieredPriceBuilder.kt
+++ src/main/kotlin/com/tomaszezula/stripe101/examples/payments/TieredPriceBuilder.kt
@@ -1,5 +1,6 @@
 package com.tomaszezula.stripe101.examples.payments
 
+import com.stripe.Stripe
 import com.stripe.model.Price
 
 typealias PriceTier = Map<String, Any>
@@ -7,9 +8,9 @@ typealias PriceTier = Map<String, Any>
 class TieredPriceBuilder {
     private var priceTiers: List<PriceTier> = mutableListOf()
 
-    fun addPriceTier(quantity: Long, unitAmount: Long): TieredPriceBuilder {
+    fun addPriceTier(quantity: Long? = null, unitAmount: Long): TieredPriceBuilder {
         val priceTier = mapOf(
-            "up_to" to quantity,
+            "up_to" to (quantity ?: "inf"),
             "unit_amount" to unitAmount
         )
         priceTiers += priceTier
@@ -22,8 +23,28 @@ class TieredPriceBuilder {
                 "currency" to currency,
                 "product" to productId,
                 "tiers_mode" to tiersMode,
-                "tiers" to priceTiers
+                "tiers" to priceTiers,
+                "recurring" to mapOf(
+                    "interval" to "month",
+                    "interval_count" to 1
+                ),
+                "billing_scheme" to "tiered"
             )
         )
     }
+}
+
+fun main() {
+    Stripe.apiKey = "your api key"
+
+    val builder = TieredPriceBuilder()
+    val price = builder
+        .addPriceTier(quantity = 5, unitAmount = 1000)
+        .addPriceTier(unitAmount = 750)
+        .build(
+            currency = "usd",
+            productId = "prod_OgmmKvjqDCDvcM",
+            tiersMode = "graduated"
+        )
+    println(price)
 }
